dd7e79bfb3c7cd2f10d60c844bc866573eed7119
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-04-26 11:47:11 -0700

    Creating contact-us btn and add style, animation and color to navbar

---
+++ src/Components/ContactUsBtn.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ContactUsBtn: React.FC = () => {
+  return (
+      <button className="contact-us-btn font-Sekuya text-xs">
+        CONTACT US
+        <div className="icon">
+          <svg height={24} width={24} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor" />
+          </svg>
+        </div>
+      </button>
+  );
+}
+
+export default ContactUsBtn;
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,27 +1,97 @@
-import { useState } from "react";
+import React from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
+import { motion, type Variants } from "framer-motion";
+import ContactUsBtn from "./Components/ContactUsBtn";
 
-export default function NavBar() {
-    const [navPages, setNavPages] = useState(["Trains", "Bookings", "Locations", "Search"]);
-    const [currentPage, setCurrentPage] = useState("Bookings");
+interface Pages {
+    page: string,
+    position: number[]
+}
+
+const navigationLiVariant: Variants = {
+    hidden: {
+        opacity: 0,
+        y: "5vh",
+    },
+
+    visible: (custom: number) => ({
+        opacity: 1,
+        y: 0,
+        transition: {delay: custom * 0.5, duration: custom * 0.5, type: "spring", mass: 2, damping: 5}
+    })
+}
+
+const navigationVariant: Variants = {
+    hidden: {
+        opacity: 0,
+        y: "15vh",
+    },
+
+    active: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.5,
+            when: "beforeChildren", 
+        },
+    }
+}
+
+const NavBar: React.FC = () => {
+    const [navPages, setNavPages] = useState<string[]>(["Trains", "Bookings", "Locations", "Search"]);
+    const [currentPage, setCurrentPage] = useState<Pages[]>([
+        {page: "Bookings", position: []}
+    ]);
+    const slideElement = useRef<HTMLDivElement | null>(null);
+    const activeNavigationElement = useRef<HTMLLIElement | null>(null); 
+
+    function activeIndicatorSlide(e: React.SyntheticEvent): void {
+        if(slideElement.current != null) {
+            const target  = e.target as HTMLUListElement;
+            setCurrentPage([{page: target.innerText, position: [target.offsetLeft, target.offsetWidth] }]);
+            slideElement.current.style.left = target.offsetLeft  + "px";
+            slideElement.current.style.width = target.offsetWidth + "px";
+        };
+    }
+
+    const resetActiveIndicator = useCallback(function resetActiveIndicator(): void {
+        if(activeNavigationElement.current && slideElement.current) {
+            slideElement.current.style.left = activeNavigationElement.current.offsetLeft  + "px";
+            slideElement.current.style.width = activeNavigationElement.current.offsetWidth + "px";
+        }
+    }, []);
+    
+    useEffect(function() {
+    window.addEventListener("load", resetActiveIndicator);
+
+     return () => {
+        window.removeEventListener("click", resetActiveIndicator);
+    };
+    }, [resetActiveIndicator])
 
     return (
-        <nav className="bg-amber-300 w-212.5 h-27.5 flex  absolute bottom-[5%] left-1/2 -translate-x-1/2 -translate-y-1/2 items-center ">
-            <div className="flex-1">
+        <nav className="w-[70%] h-27.5 flex  absolute bottom-[5%] left-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-around">
+            <div className="flex-initial mr-5">
                 <img src="favicon.svg" alt="slimes" />
             </div>
-            <div className="flex grow rounded-[70px] max-w-5xl h-22 border-2
-             border-amber-200  bg-red-400 flex-2/4">
-                <ul className="size-full flex justify-around items-center text-base">
-                    {navPages.map((element, index) => currentPage == element ? <li className='w-25 h-15 bg-blue-500 text-center rounded-[70px] py-4 font-testing-font'>{element}</li>
-                     : <li className='bg-transparent cursor-pointer' onClick={() => setCurrentPage(element)}>{element}</li>)}
-                </ul>
+            <motion.div variants={navigationVariant} initial="hidden" animate="active" className="flex rounded-[70px] max-w-5xl h-22 flex-3/4
+            bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 border-2 border-gray-100">
+                <motion.ul initial="hidden" animate="visible" className="size-full flex items-center px-2 relative text-white">
+                    <div className="h-15 glass rounded-[70px] absolute transition-all duration-500 ease-in-out" ref={slideElement}></div>
 
-            </div>
-            <div className="flex-1 h-full flex item-center ml-5">
-                <button className="w-45 h-22 bg-black text-amber-50 
-                rounded-[45px] m-auto text-2xl font-serif">Contact Us</button>
+                    {navPages.map((element, index) => currentPage[0].page == element ? <motion.li custom={index + 1} variants={navigationLiVariant} className='z-10 text-center text-base font-Sekuya flex-1' 
+                     ref={activeNavigationElement} key={index}>{element}</motion.li>
+
+                     : <motion.li custom={index + 1} variants={navigationLiVariant} className='cursor-pointer flex-1 z-10 text-center font-Playfair text-lg' onClick={(e) => activeIndicatorSlide(e)} key={index}>{element}</motion.li>)}
+                </motion.ul>
+
+            </motion.div>
+            <div className="flex-initial ml-5">
+                <ContactUsBtn/>
             </div>
 
         </nav>
     );
 }
+
+export default NavBar;
