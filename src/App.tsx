de6d2f9ac86908c9677066ee433927e37d0d2fa6
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-04-27 23:45:06 -0700

    creating header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import NavBar from "./Navbar";
+// import NavBar from "./Navbar";
+import Header from "./Header";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="w-full h-screen darkMode">
-      <NavBar/>
-
+      {/* <NavBar/> */}
+      <div className="size-full overflow-x-hidden overflow-y-scroll">
+        <Header/>
+      </div>
 
     </div>
   );
