fc728ca6f9389cd2a6c5c6da0e3d224ce9ab6bd8
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-05-02 22:34:08 -0700

    Customizing header form

--- src/Components/HeaderForm.tsx
+++ src/Components/HeaderForm.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { WiTrain } from "react-icons/wi";
+import { LuTrainTrack } from "react-icons/lu";
+import { IconContext } from "react-icons";
+import { IoTrainOutline } from "react-icons/io5";
+import { BsCalendar2DateFill } from "react-icons/bs";
+import { TiGroup } from "react-icons/ti";
+import { CgRedo } from "react-icons/cg";
+import { TbTrainFilled } from "react-icons/tb";
 
 const HeaderForm: React.FC = () => {
+  const [journeyRoute, setJourneyRoute] = useState<string>("oneWay");
+  
   return (
       <div className="form-container">
         <div className="form-content">
+          <IconContext.Provider value={{"size": "3rem"}}>
           <div className="title-section">
-            <svg className="title-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-              <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-            </svg>
-            <p className="title">Dossier</p>
+            <WiTrain/>
+            <p className="title">Express-Train</p>
           </div>
+          </IconContext.Provider>
           <form className="form">
 
             {/* First input field */}
             <div className="input-group">
+              <IconContext.Provider value={{"size": "2rem"}}>
               <div className="input-icon">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-                  <circle cx={12} cy={7} r={4} />
-                </svg>
+                <LuTrainTrack/>
               </div>
+              </IconContext.Provider>
               <div className="input-content">
                 <label htmlFor="FromCity">From</label>
                 <input type="text" name="FromCity" id="FromCity" placeholder="City or Station" />
@@ -29,10 +38,9 @@ const HeaderForm: React.FC = () => {
             {/* Second input field */}
             <div className="input-group">
               <div className="input-icon">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <rect x={3} y={11} width={18} height={11} rx={2} ry={2} />
-                  <path d="M7 11V7a5 5 0 0110 0v4" />
-                </svg>
+                <IconContext.Provider value={{"size": "2rem"}}>
+                  <IoTrainOutline/>
+                </IconContext.Provider>
               </div>
               <div className="input-content">
                 <label htmlFor="ToCity">To</label>
@@ -41,48 +49,76 @@ const HeaderForm: React.FC = () => {
             </div>
 
 
-            <div className="journeySwitchBtn-div">
-                <button>One-way</button>
-                <button>Return</button>
-            </div>
+            <div className="w-[90%] h-18.75 flex justify-between items-center self-center">
+
+                <button className={`w-50 h-12.5 text-sm font-Sekuya text-black rounded-xl ml-2 cursor-pointer
+                 ${journeyRoute != 'return' ? 'darkMode-form-btn-color1 cursor-not-allowed!' : 'darkMode-form-btn-color' }`}
+                    onClick={() => setJourneyRoute("oneWay")} type="button">One-way</button>
+
+
+                <button className={`w-50 h-12.5 text-sm font-Sekuya text-black rounded-xl ml-2 cursor-pointer
+                 ${journeyRoute != 'oneWay' ? 'darkMode-form-btn-color1 cursor-not-allowed!' : 'darkMode-form-btn-color' }`}
+                  onClick={() => setJourneyRoute("return")} type="button">Return</button>
+
+          </div>
 
             {/* Third input field */}
             <div className="input-group">
               <div className="input-icon">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <rect x={3} y={11} width={18} height={11} rx={2} ry={2} />
-                  <path d="M7 11V7a5 5 0 0110 0v4" />
-                </svg>
+                <IconContext.Provider value={{"size": "2rem"}}>
+                  <BsCalendar2DateFill />
+                </IconContext.Provider>
               </div>
               <div className="input-content">
-                <label htmlFor="TravelDate">Password</label>
+                <label htmlFor="TravelDate">Out</label>
+                <input type="text" name="TravelDate" id="TravelDate" placeholder="Mon 27 April . 10:15 PM" />
+              </div>
+            </div>
+
+
+            <div className={`input-group ${journeyRoute == 'oneWay' && 'hidden!'}`}>
+              <div className="input-icon">
+                <IconContext.Provider value={{"size": "2rem"}}>
+                  <CgRedo />
+                </IconContext.Provider>
+              </div>
+              <div className="input-content">
+                <label htmlFor="TravelDate">Return</label>
                 <input type="text" name="TravelDate" id="TravelDate" placeholder="Mon 27 April . 10:15 PM" />
               </div>
             </div>
 
 
             {/* Fourth input field */}
-            <div className="input-group">
-              <div className="input-icon">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <rect x={3} y={11} width={18} height={11} rx={2} ry={2} />
-                  <path d="M7 11V7a5 5 0 0110 0v4" />
-                </svg>
+
+            <div className="w-full h-21.25 border border-darkMode-border-color flex cursor-pointer hover:border-darkMode-border-color1">
+              <div className="flex-initial flex items-center pl-3!">
+                 <div className="input-icon">
+                    <IconContext.Provider value={{"size": "2rem"}}>
+                      <TiGroup />
+                    </IconContext.Provider>
+                  </div>
               </div>
-              <div className="input-content">
-                <label htmlFor="ReturnDate">Password</label>
-                <input type="text" name="ReturnDate" id="ReturnDate" placeholder="Web 29 April . 1:15 AM" />
+
+
+              <div className="flex-1 flex items-center gap-2 justify-center">
+                <p className="font-Playfair text-xl">7 Adult</p>
+              </div>
+
+              <div className="flex-initial flex items-center w-10 justify-center">
+                <h1 className="text-2xl">&#10095;</h1>
               </div>
             </div>
 
-            <div className="InformationBtnDiv">
-                <button>2 Adults</button>
-            </div>
 
-
+            {/* Submit Button */}
             <button className="sign" type="submit">
               <span>Sign In</span>
-              <span className="sign-arrow">→</span>
+              <span className="sign-arrow">
+                <IconContext.Provider value={{"size": "1.12rem"}}>
+                    <TbTrainFilled />
+                </IconContext.Provider>
+              </span>
             </button>
           </form>
         
