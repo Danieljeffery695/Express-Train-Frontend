b5bedb15f73b2b42ed9e039655cc4f3b805f9e87
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-05-12 22:17:54 -0700

    Creating section two

---
+++ src/SectionTwo.tsx
@@ -0,0 +1,13 @@
+// I am thinking of using a video background with some included scrollTrigger behaviors.
+// I will create a slider of photo. when user scroll the slide should move to the next one and the video play the next chapter
+// The video will illustrate some key city's and vacations site around the selected slide location
+
+import React from "react";
+
+const SectionTwo: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SectionTwo;
