052b90386694fdf00e2c70d4e9e33d17a083d08c
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-06-06 22:15:19 -0700

    Finally Creating footer

---
+++ src/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return <div>
+        <p>Next Stop</p>
+        <div>
+            <img src="" alt="photo" />
+            <h1>ADAPTIVE SKILLS TRAINING</h1>
+            <img src="" alt="photo" />
+            <img src="" alt="photo" />
+        </div>
+
+        <div>
+            
+        </div>
+    </div>
+}
+
+export default Footer;
